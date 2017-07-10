d2f045e1ccfdafd4d7695ee175c5e9b060ef5e4c
German Boggio <german.boggio@cube.com.ar>
2017-07-10 20:47:32 -0300

    Add store model

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
 
-// Do work here
 router.get('/', storeController.homePage);
 
 module.exports = router;
--- start.js
+++ start.js
@@ -12,7 +12,7 @@ mongoose.connection.on('error', (err) => {
 });
 
 // READY?! Let's go!
-
+// require('./models/Store');
 
 // Start our app!
 const app = require('./app');
